f2ffc2a0a45234013619dd2658fbc006bddca0cc
mayk05 <mishha007@ya.ru>
2024-06-16 21:34:23 +0400

    update MoveList

--- src/components/MoveList/MoveList.tsx
+++ src/components/MoveList/MoveList.tsx
@@ -1,10 +1,14 @@
 import React, {FC, SyntheticEvent, useState} from 'react';
-import {useChessBoard} from "../../containers/ChessBoardContext";
-import {Alert, Box, Snackbar, Typography, useTheme} from "@mui/material";
+import {useChessBoard} from '../../containers/ChessBoardContext';
+import {Accordion, AccordionDetails, AccordionSummary, Alert, Box, Snackbar, Typography, useTheme} from '@mui/material';
+import {ExpandMore as ExpandMoreIcon} from '@mui/icons-material';
+import Draggable from 'react-draggable';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const MoveList: FC = () => {
     const {chessboard} = useChessBoard();
     const theme = useTheme();
+    const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
     const [open, setOpen] = useState(false);
     const formattedMoves = chessboard.getLetterRepresentation().reduce((acc: string[], move: string, index: number) => {
         const chunkIndex = Math.floor(index / 5);
@@ -30,33 +34,58 @@ const MoveList: FC = () => {
         }
         setOpen(false);
     };
+
     return (
-        <Box >
-            <Typography variant="h4" className="mt5" gutterBottom>Moves</Typography>
-            <Box mb={2}>
-                {formattedMoves && formattedMoves.map((row, index) => (
-                    <Typography key={index} variant="body1">{row}</Typography>
-                ))}
-            </Box>
-            {chessboard.tourPath?.length === 0 && chessboard.completed && (
-                <Box sx={{ textAlign: 'center' }}>
-                    <Typography variant="body2" sx={{ color: theme.palette.error.main }}>
-                        Not completed!
-                    </Typography>
+        <>
+            <Draggable disabled={!isDesktop}>
+                <Box
+                    sx={{
+                        position: isDesktop ? 'fixed' : 'default',
+                        padding: theme.spacing(2),
+                        zIndex: 1000,
+                        cursor: 'move',
+                        bottom: 0,
+                        right: 0,
+                        minWidth: isDesktop?200:'100%',
+                        minHeight: isDesktop?200:'100%',
+                    }}
+                >
+                    <Accordion defaultExpanded>
+                        <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                            <Typography variant="h5" width="100%" textAlign="center">
+                                Moves
+                            </Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Box mb={2}>
+                                {formattedMoves && formattedMoves.map((row, index) => (
+                                    <Typography key={index} variant="body1">{row}</Typography>
+                                ))}
+                            </Box>
+                            {chessboard.tourPath?.length === 0 && chessboard.completed && (
+                                <Box sx={{textAlign: 'center'}}>
+                                    <Typography variant="body2" sx={{color: theme.palette.error.main}}>
+                                        Not completed!
+                                    </Typography>
+                                </Box>
+                            )}
+                        </AccordionDetails>
+                    </Accordion>
                 </Box>
-            )}
+            </Draggable>
+
             <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}
-                      anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}>
+                      anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}>
                 <Alert
                     onClose={handleClose}
                     severity="warning"
                     variant="filled"
-                    sx={{ width: '100%' }}
+                    sx={{width: '100%'}}
                 >
                     No way found. Try another starting point!
                 </Alert>
             </Snackbar>
-        </Box>
+        </>
     );
 };
 
